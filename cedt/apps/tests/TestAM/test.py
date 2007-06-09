753e123a75c3093f5fcc11fa4480604281aa8c71
venkatesh2012 <venkatesh2012>
2007-06-09 16:09:33 +0000

    TestAM application

---
+++ cedt/apps/tests/TestAM/test.py
@@ -0,0 +1,55 @@
+from TOSSIM import *
+from random import *
+import sys
+
+t = Tossim([])
+r = t.radio()
+f = open("topo.txt", "r")
+
+lines = f.readlines()
+for line in lines:
+  s = line.split()
+  if (len(s) > 0):
+    print " ", s[0], " ", s[1], " ", s[2];
+    r.add(int(s[0]), int(s[1]), float(s[2]))
+
+for i in range(0, 2):
+  m = t.getNode(i);
+  time = randint(t.ticksPerSecond(), 10 * t.ticksPerSecond())
+  m.bootAtTime(t.ticksPerSecond())
+  print "Booting ", i, " at time ", time
+
+
+noise = open("rssi-log-mica2.txt", "r")
+lines = noise.readlines()
+for line in lines:
+	str = line.strip()
+	if (str != ""):
+		val = float(str)
+		for i in range(0, 2):
+			t.getNode(i).addNoiseTraceReading(int(val))
+
+for i in range(0, 2):
+	print "Creating noise model for ",i;
+	t.getNode(i).createNoiseModel()
+
+t.addChannel("TestAM",sys.stdout);
+#t.addChannel("HplCC1000P",sys.stdout);
+#t.addChannel("HplCC1KSpiWrite",sys.stdout);
+#t.addChannel("HplCC1KSpiRead",sys.stdout);
+#t.addChannel("HplCC1KSpi",sys.stdout);
+#t.addChannel("CC1000SendReceiveP",sys.stdout);
+#t.addChannel("HplAtm128AdcP",sys.stdout);
+#t.addChannel("CC1000CsmaP",sys.stdout);
+
+t.runNextEvent();
+time = t.time();
+
+#simulate for Min * Sec * ticksPerSecond  seconds 
+
+while(time+1*50*t.ticksPerSecond() > t.time()) :
+    t.runNextEvent()
+
+print "Completed simulation for",t.time()/t.ticksPerSecond(),"sec." 
+
+

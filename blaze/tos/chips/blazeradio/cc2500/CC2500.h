cbd5817b3335bf304c0b0513fa931a7dcaf38e68
jayrodhill <jayrodhill>
2007-11-02 21:31:44 +0000

    changed gdo2 to assert when rxfifo > fifothreshold (0x01)
    changed fifothreshold = 0x0F

--- blaze/tos/chips/blazeradio/cc2500/CC2500.h
+++ blaze/tos/chips/blazeradio/cc2500/CC2500.h
@@ -47,7 +47,8 @@ enum {
 
 enum cc2500_config_reg_state_enums {
   /** GDO2 asserts at Rx sync and deasserts at end of packet */
-  CC2500_CONFIG_IOCFG2 = 0x06,
+  CC2500_CONFIG_IOCFG2 = 0x01,
+  //CC2500_CONFIG_IOCFG2 = 0x06,
   
   /** GDO1 is High Impedance */
   CC2500_CONFIG_IOCFG1 = 0x2E,
@@ -55,7 +56,8 @@ enum cc2500_config_reg_state_enums {
   /** GDO0 goes high when the channel is clear */
   CC2500_CONFIG_IOCFG0 = 0x09, 
   
-  CC2500_CONFIG_FIFOTHR = 0x07,
+  CC2500_CONFIG_FIFOTHR = 0x0F,
+  //CC2500_CONFIG_FIFOTHR = 0x07,
   CC2500_CONFIG_SYNC1 = 0xD3,
   CC2500_CONFIG_SYNC0 = 0x91,
   

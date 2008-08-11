364e27f32e4d34a612216ebe9c46c8e5457c5624
kusy <kusy>
2008-08-11 16:48:04 +0000

    *** empty log message ***

---
+++ stanford-lgl/apps/tests/TestPacketTimeStamp/TestPacketTimeStampP.nc
@@ -0,0 +1,125 @@
+/*
+* Copyright (c) 2006 Stanford University.
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions
+* are met:
+* - Redistributions of source code must retain the above copyright
+*   notice, this list of conditions and the following disclaimer.
+* - Redistributions in binary form must reproduce the above copyright
+*   notice, this list of conditions and the following disclaimer in the
+*   documentation and/or other materials provided with the
+*   distribution.
+* - Neither the name of the Stanford University nor the names of
+*   its contributors may be used to endorse or promote products derived
+*   from this software without specific prior written permission
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+* ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+* LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+* FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL STANFORD
+* UNIVERSITY OR ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+* INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+* SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+* HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+* STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+* ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+* OF THE POSSIBILITY OF SUCH DAMAGE.
+*/ 
+/**
+ * @author Brano Kusy (branislav.kusy@gmail.com)
+ * - radio message timestamping test application
+ * - tests both message reception and transmission timestamps
+ * - each node periodically broadcasts TimeStampPoll messages which are
+ *   timestamped at both sender and receiver. TimeStampPoll message includes
+ *   previous timestamp of the sender. receiver reports both reception timestamp
+ *   and the previous sender timestamp.
+ * - BusyTimer fires periodically to simulate CPU load
+ */
+
+#include "TestPacketTimeStamp.h"
+
+module TestPacketTimeStampP
+{
+    uses 
+    {
+        interface Boot;
+        interface Leds;
+
+        interface SplitControl as RadioControl;
+        interface AMSend as ReportSend;
+        interface AMSend as PollSend;
+        interface Receive as PollReceive;
+        interface PacketTimeStamp<T32khz, uint32_t>;
+
+        interface Timer<TMilli> as BcastTimer;
+        interface Timer<TMilli> as BusyTimer;
+    }
+}
+
+implementation
+{
+    #ifndef TIMESYNC_POLLER_RATE
+    #define TIMESYNC_POLLER_RATE 250
+    #endif
+
+    message_t msg, msgReport;
+
+    event void Boot.booted() {
+      TimeStampPoll *tsp = call PollSend.getPayload(&msg, sizeof(TimeStampPoll));
+      TimeStampPollReport *tspr =
+        call ReportSend.getPayload(&msgReport, sizeof(TimeStampPollReport));
+      
+      tsp->senderAddr = TOS_NODE_ID;
+      tsp->msgID = 0;
+      tsp->previousSendTime = 0;
+      tspr->localAddr = TOS_NODE_ID;
+
+      call RadioControl.start();
+      call BcastTimer.startPeriodic(TIMESYNC_POLLER_RATE);
+      call BusyTimer.startPeriodic(30);//every 30ms, do some work
+    }
+
+    event void BcastTimer.fired(){
+        TimeStampPoll *tsp = call PollSend.getPayload(&msg, sizeof(TimeStampPoll));
+        ++(tsp->msgID);
+        call PollSend.send(AM_BROADCAST_ADDR, &msg, sizeof(TimeStampPoll));
+        call Leds.led0Toggle();
+    }
+
+    task void foo(){
+        uint32_t time = call BusyTimer.getNow();
+        while ((call BusyTimer.getNow() - time) < 2);//keep CPU busy for up to 2ms
+    }
+
+    event void BusyTimer.fired(){
+        post foo();
+    }
+
+    event message_t* PollReceive.receive(message_t* p, void* payload, uint8_t len){
+        TimeStampPoll *tsp = (TimeStampPoll *)payload;
+        TimeStampPollReport *tspr =
+            call ReportSend.getPayload(&msgReport, sizeof(TimeStampPollReport));
+           
+        call Leds.led1Toggle();
+        tspr->senderAddr = tsp->senderAddr;
+        tspr->msgID = tsp->msgID;
+        tspr->previousSendTime = tsp->previousSendTime;
+
+        tspr->receiveTime = call PacketTimeStamp.timestamp(p);
+        call ReportSend.send(AM_BROADCAST_ADDR, &msgReport, sizeof(TimeStampPollReport));
+
+        return p;
+    }
+
+    event void RadioControl.startDone(error_t error){};
+    event void RadioControl.stopDone(error_t error){};
+    event void PollSend.sendDone(message_t* p, error_t success){
+        TimeStampPoll *tsp = call PollSend.getPayload(p, sizeof(TimeStampPoll));
+        tsp->previousSendTime = call PacketTimeStamp.timestamp(p);
+    };
+    event void ReportSend.sendDone(message_t* p, error_t success){};
+
+}
---
+++ stanford-lgl/apps/tests/TestPacketTimeStamp/data/DataLogger.java
@@ -0,0 +1,231 @@
+/*									tab:4
+ * "Copyright (c) 2000-2003 The Regents of the University  of California.  
+ * All rights reserved.
+ *
+ * Permission to use, copy, modify, and distribute this software and its
+ * documentation for any purpose, without fee, and without written agreement is
+ * hereby granted, provided that the above copyright notice, the following
+ * two paragraphs and the author appear in all copies of this software.
+ * 
+ * IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY FOR
+ * DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES ARISING OUT
+ * OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF THE UNIVERSITY OF
+ * CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS FOR A PARTICULAR PURPOSE.  THE SOFTWARE PROVIDED HEREUNDER IS
+ * ON AN "AS IS" BASIS, AND THE UNIVERSITY OF CALIFORNIA HAS NO OBLIGATION TO
+ * PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS."
+ *
+ * Copyright (c) 2002-2007 Intel Corporation
+ * All rights reserved.
+ *
+ * This file is distributed under the terms in the attached INTEL-LICENSE     
+ * file. If you do not find these files, copies can be found by writing to
+ * Intel Research Berkeley, 2150 Shattuck Avenue, Suite 1300, Berkeley, CA, 
+ * 94704.  Attention:  Intel License Inquiry.
+ */
+
+
+/**
+ * @author Brano Kusy
+ */
+
+import java.io.BufferedReader;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+import java.util.TreeMap;
+
+import net.tinyos.message.*;
+import net.tinyos.util.*;
+
+public class DataLogger implements MessageListener {
+	public class RunWhenShuttingDown extends Thread { 
+		public void run() 
+		{ 
+			System.out.println("TX/RX:\tErrB\tErrS\tAvgErrS\t \tErrB2\tErrS2\tAvgErrS2");
+			for (TxRx id:rx_node_times.keySet())
+				System.out.println(id+"\t "+rx_node_times.get(id));
+			if (outPoll!=null)
+				outPoll.close();
+			if (outReport!=null)
+				outReport.close();
+		} 
+	}
+	
+	MoteIF mote; 
+
+    void connect()
+    {
+		try {
+		    mote = new MoteIF(PrintStreamMessenger.err);
+		    mote.registerListener(new TimeStampPoll(), this);
+		    mote.registerListener(new TimeStampPollReport(), this);
+		    System.out.println("Connection ok!");
+
+			String name=""+System.currentTimeMillis();
+			outPoll = new PrintStream(new FileOutputStream(name+".poll"));
+			outReport = new PrintStream(new FileOutputStream(name+".report"));
+    		outPoll.println("#[SENDER_ID] [MSG_ID] [PREV_SEND_TIME]");
+    		outReport.println("#[RECEIVER_ID] [SENDER_ID] [MSG_ID] [PREV_SEND_TIME] [RX_TIME]");
+		}
+		catch(Exception e) {
+		    e.printStackTrace();
+		    System.exit(2);
+		}
+    }
+    PrintStream outPoll = null;
+    PrintStream outReport = null;
+    
+    final int RX_ERROR=5;
+    class NodeTimes {
+    	TreeMap<Integer, Long> time = new TreeMap<Integer, Long>();//indexed by msg_id
+        int numSmallErrors=0, numBigErrors=0;
+        double sumSmallErrors=0d;
+    	public String toString() {
+    		return new String(""+numBigErrors+"\t"+numSmallErrors+"\t"
+    							+((int)(sumSmallErrors*1000/numSmallErrors))/1000f);
+    	}
+    }
+    
+    class TxRx {
+    	Integer tx;
+    	Integer rx;
+    	public TxRx(int tx, int rx) { this.tx=tx; this.rx=rx;}
+    	public String toString() {return new String(tx+"/"+rx);}
+    	public boolean equals (Object o) {
+    		if (o instanceof TxRx) {
+				TxRx other = (TxRx) o;
+				if (other.tx==tx && other.rx==rx)
+					return true;
+			}
+    		return false;
+    	}
+    	public int hashCode() {
+    		return tx.hashCode()+255*rx.hashCode(); 
+    	}
+    }
+    
+    HashMap<Integer,NodeTimes> tx_node_times = new HashMap<Integer,NodeTimes>();//indexed by tx_id
+    HashMap<TxRx,NodeTimes> rx_node_times = new HashMap<TxRx,NodeTimes>();//indexed by tx_id,rx_id
+    
+    public DataLogger() {
+		Runtime.getRuntime().addShutdownHook(new RunWhenShuttingDown());
+    }
+
+	public void writePoll(TimeStampPoll tsp)
+	{
+		outPoll.println(tsp.get_senderAddr()+" "+tsp.get_msgID()+" "+tsp.get_previousSendTime());
+		outPoll.flush();
+	}
+	    
+	public void findRxError (int txId, int rxId, int msgId)
+	{
+		NodeTimes rxTimes = rx_node_times.get(new TxRx(txId,rxId));
+		NodeTimes txTimes = tx_node_times.get(txId);
+		Integer prevMsgId = rxTimes.time.lowerKey(msgId);
+		if (prevMsgId == null || !txTimes.time.containsKey(prevMsgId))
+			return;
+		Long rxTimePrev = rxTimes.time.get(prevMsgId);
+		Long rxTime = rxTimes.time.get(msgId);
+		Long txTimePrev = txTimes.time.get(prevMsgId);
+		Long txTime = txTimes.time.get(msgId);
+		if (rxTimePrev!=null&&rxTime!=null&&txTimePrev!=null&&txTime!=null)
+		{
+		    double exp_rx_time = rxTimePrev+txTime-txTimePrev;
+		    double rx_error = exp_rx_time-rxTime;
+		    if (Math.abs(rx_error)>RX_ERROR){
+		    	++rxTimes.numBigErrors;
+		    	System.out.println("RX error: rxId,txId ["+rxId+","+txId+"], msg "+msgId+", exp_time "+exp_rx_time+", time "+rxTime+" (error "+rx_error+")");
+		    }
+		    else {
+		    	++rxTimes.numSmallErrors;
+		    	rxTimes.sumSmallErrors+=rx_error;
+		    	if (rxTimes.numSmallErrors%100==0)
+		    		System.out.println("Node "+txId+": "+rxTimes.numSmallErrors+" RX timestamps without error (avg "+(rxTimes.sumSmallErrors/rxTimes.numSmallErrors)+")");
+		    }		
+		}
+	}
+	
+	public void analyzeReprot(int txId, int rxId, int msgId, long tx_prev_time, long rx_time)
+	{
+		NodeTimes txTimes = tx_node_times.get(txId);
+		NodeTimes rxTimes = rx_node_times.get(new TxRx(txId, rxId));
+		if (txTimes == null) {
+			txTimes = new NodeTimes();
+			tx_node_times.put(txId, txTimes);			
+		}
+		if (rxTimes == null) {
+			rxTimes = new NodeTimes();
+			rx_node_times.put(new TxRx(txId,rxId),rxTimes);
+		}
+		
+		txTimes.time.put(msgId-1, tx_prev_time);
+		rxTimes.time.put(msgId, rx_time);
+		
+		//look at the error of the prev msg (tx sends tx_prev_time)
+		if (rxTimes.time.containsKey(msgId-1))
+			findRxError(txId, rxId, msgId-1);
+	}
+	    
+    public void messageReceived(int dest_addr, Message msg) 
+    {
+		if (msg instanceof TimeStampPoll)
+			writePoll((TimeStampPoll)msg);
+		else if (msg instanceof TimeStampPollReport)
+		{
+			TimeStampPollReport tspr=(TimeStampPollReport)msg;
+			int txId = tspr.get_senderAddr();
+			int rxId = tspr.get_localAddr();
+			int msgId = tspr.get_msgID();
+			long tx_prev_time = tspr.get_previousSendTime();
+			long rx_time = tspr.get_receiveTime();
+			outReport.println(rxId+" "+txId+" "+msgId+" "+tx_prev_time+" "+rx_time);
+			outReport.flush();
+
+			analyzeReprot(txId, rxId, msgId, tx_prev_time, rx_time);
+		}
+    }
+        
+    void loadData(String filename) throws Exception
+    {
+    	FileReader myFile = new FileReader(filename);
+    	BufferedReader myReader = new BufferedReader(myFile);
+    	String fileLine;
+    	while ( (fileLine = myReader.readLine()) != null )
+    	{
+    		if (!fileLine.startsWith("#"))
+    		{
+				  StringTokenizer tokenizer = new StringTokenizer(fileLine);
+				  int rxId=Integer.parseInt(tokenizer.nextToken());
+				  int txId=Integer.parseInt(tokenizer.nextToken());
+				  int msgId=Integer.parseInt(tokenizer.nextToken());
+				  long tx_prev_time=Long.parseLong(tokenizer.nextToken());
+				  long rx_time=Long.parseLong(tokenizer.nextToken());
+				  analyzeReprot(txId, rxId, msgId, tx_prev_time, rx_time);
+    		}
+    	}
+    }
+    /* If no params are given, app will start in the logging mode
+     * Filename of a '.report' file can be given as a parameter
+     * to execute error analysis on the previously saved data.
+     */
+    public static void main(String[] args) {
+		DataLogger logger = new DataLogger();
+    	if (args.length==0)
+    		logger.connect();
+    	else
+    		try
+	    	{
+    			System.out.println("Reading file: "+args[0]+"\n");
+    			logger.loadData(args[0]);
+	    	} catch (Exception e) {
+	    		e.printStackTrace();
+	    	}
+    }
+}

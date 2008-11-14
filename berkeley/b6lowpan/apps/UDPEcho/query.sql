a0c814f8f40e7ae8b5fcc0638766479200b46321
sdhsdh <sdhsdh>
2008-11-14 01:36:54 +0000

    remove old version

--- berkeley/b6lowpan/apps/UDPEcho/query.sql
+++
@@ -1,3 +0,0 @@
-SELECT hex(origin), count(DISTINCT udp_seqno) + max(udp_failed)  AS realsent, max(udp_seqno) AS sn, max(udp_failed), route_parent_hops AS hlim, route_parent_neighbor AS neighbor, max(ip_forwarded), max(ip_tx_drop) AS td, max(ip_fw_drop) AS fd, max(ip_rx_drop) AS rd, max(ip_real_drop) AS rd, max(ip_hlim_drop) AS hd, max(ip_senddone_el) AS el FROM lineMM WHERE 1 GROUP BY origin ORDER BY route_parent_hops,origin,ts;
-
-SELECT origin, max(udp_failed)+count(DISTINCT udp_seqno),max(udp_seqno) FROM lineMM GROUP BY origin INTO OUTFILE '/tmp/histo'; 
--- berkeley/b6lowpan/tos/lib/net/b6lowpan/nwprog/BootImage.nc
+++
@@ -1,5 +0,0 @@
-
-interface BootImage {
-  command void reboot();
-  command error_t boot(uint8_t img_num);
-}

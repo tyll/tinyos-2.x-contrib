67f039f1dc8c98adf7456cd92dfac50ce5c9c065
rfonseca76 <rfonseca76>
2009-03-01 04:34:17 +0000

    Added a directory to the PATH in quanto_setup

--- berkeley/quanto/quanto_setup.sh
+++ berkeley/quanto/quanto_setup.sh
@@ -1,4 +1,4 @@
 export QUANTO_ROOT=`pwd`
 export TOSMAKE_PATH="$QUANTO_ROOT/support/make"
-export PATH=$QUANTO_ROOT/tools/quanto/labjack:$QUANTO_ROOT/tools/quanto/scripts:$PATH
+export PATH=$QUANTO_ROOT/tools/quanto/compression:$QUANTO_ROOT/tools/quanto/labjack:$QUANTO_ROOT/tools/quanto/scripts:$PATH
 

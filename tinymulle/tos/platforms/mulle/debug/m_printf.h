b3cecab66344e0122e33c526b38777b2dae17dd3
r-studio <r-studio>
2009-05-16 18:54:15 +0000

    *** empty log message ***

--- tinymulle/tos/platforms/mulle/debug/m_printf.h
+++ tinymulle/tos/platforms/mulle/debug/m_printf.h
@@ -1,22 +1,22 @@
-// $Id$
 /*
- * Copyright (c) 2009 EISLAB (www.ltu.se)
- * All rights reserved.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published 
- * by the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+	Copyright 2001, 2002 Georges Menie (www.menie.org)
 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
 
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- */
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+
 
 /**
  * The file is a debug tool for the Mull-TinyOS platform. To use this tool,
@@ -29,6 +29,7 @@
  * the output, for example, cutecom in linux.
  * For more information see www.eistec.se.
  *
+ * Modification to adopt printf for tinymulle is made by
  * @author Fan Zhang
  * @author Cheng Zhong
  */

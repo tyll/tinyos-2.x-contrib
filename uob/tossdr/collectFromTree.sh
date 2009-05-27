00971e5be8be49ff2c9e6cd9d459b80e445ab38e
mab-cn <mab-cn>
2009-05-27 22:54:18 +0000

    Fix to/from tree scripts.

--- uob/tossdr/collectFromTree.sh
+++ uob/tossdr/collectFromTree.sh
@@ -5,6 +5,12 @@ mkdir -p tos/platforms/micaz/sdr/
 cp /opt/tinyos-2.x/tos/platforms/micaz/sdr/.platform tos/platforms/micaz/sdr/
 cp /opt/tinyos-2.x/tos/platforms/micaz/sdr/platform_hardware.h tos/platforms/micaz/sdr/
 
+mkdir -p tos/platforms/mica/sdr/
+cp $TOSROOT/tos/platforms/mica/sdr/* tos/platforms/mica/sdr/
+
+mkdir -p tos/platforms/micaz/chips/cc2420/sdr/
+cp $TOSROOT/tos/platforms/micaz/chips/cc2420/sdr/* tos/platforms/micaz/chips/cc2420/sdr/
+
 mkdir -p tos/chips/cc2420/sdr/
 cp /opt/tinyos-2.x/tos/chips/cc2420/sdr/*.nc tos/chips/cc2420/sdr/
 
@@ -28,6 +34,15 @@ cp $TOSROOT/tos/lib/tossdr/* tos/lib/tossdr/
 
 mkdir -p tos/chips/atm128/sdr
 cp $TOSROOT/tos/chips/atm128/sdr/* tos/chips/atm128/sdr/
+
+mkdir -p tos/chips/atm128/pins/sdr
+cp $TOSROOT/tos/chips/atm128/pins/sdr/* tos/chips/atm128/pins/sdr/
+
+mkdir -p tos/chips/atm128/spi/sdr
+cp $TOSROOT/tos/chips/atm128/spi/sdr/* tos/chips/atm128/spi/sdr/
+
+mkdir -p tos/chips/atm128/timer/sdr
+cp $TOSROOT/tos/chips/atm128/timer/sdr/* tos/chips/atm128/timer/sdr/
 
 # copy SDR target from tree
 mkdir -p support/make/
--- uob/tossdr/installIntoTree.sh
+++ uob/tossdr/installIntoTree.sh
@@ -4,6 +4,12 @@ mkdir -p $TOSROOT/tos/platforms/micaz/sdr/
 cp tos/platforms/micaz/sdr/.platform $TOSROOT/tos/platforms/micaz/sdr/
 cp tos/platforms/micaz/sdr/platform_hardware.h $TOSROOT/tos/platforms/micaz/sdr/
 
+mkdir -p $TOSROOT/tos/platforms/mica/sdr/
+cp tos/platforms/mica/sdr/* $TOSROOT/tos/platforms/mica/sdr/
+
+mkdir -p $TOSROOT/tos/platforms/micaz/chips/cc2420/sdr/
+cp tos/platforms/micaz/chips/cc2420/sdr/* $TOSROOT/tos/platforms/micaz/chips/cc2420/sdr/
+
 mkdir -p $TOSROOT/tos/chips/cc2420/sdr/
 cp tos/chips/cc2420/sdr/*.nc $TOSROOT/tos/chips/cc2420/sdr/
 
@@ -27,6 +33,15 @@ cp tos/lib/tossdr/* $TOSROOT/tos/lib/tossdr/
 
 mkdir -p $TOSROOT/tos/chips/atm128/sdr
 cp tos/chips/atm128/sdr/* $TOSROOT/tos/chips/atm128/sdr/
+
+mkdir -p $TOSROOT/tos/chips/atm128/pins/sdr
+cp tos/chips/atm128/pins/sdr/* $TOSROOT/tos/chips/atm128/pins/sdr/
+
+mkdir -p $TOSROOT/tos/chips/atm128/spi/sdr
+cp tos/chips/atm128/spi/sdr/* $TOSROOT/tos/chips/atm128/spi/sdr/
+
+mkdir -p $TOSROOT/tos/chips/atm128/timer/sdr
+cp tos/chips/atm128/timer/sdr/* $TOSROOT/tos/chips/atm128/timer/sdr/
 
 mkdir -p $TOSROOT/support/make/
 cp support/make/sdr.extra $TOSROOT/support/make/

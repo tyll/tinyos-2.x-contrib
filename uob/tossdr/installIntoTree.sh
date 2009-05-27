5ce22cb90fb03387f319074d1bb784de9cb57f4a
mab-cn <mab-cn>
2009-05-27 20:58:55 +0000

    Fixed installIntoTree.sh

--- uob/tossdr/installIntoTree.sh
+++ uob/tossdr/installIntoTree.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
-mkdir -p tos/platforms/micaz/sdr/
-cp $(TOSROOT)/tos/platforms/micaz/sdr/.platform tos/platforms/micaz/sdr/
-cp $(TOSROOT)/tos/platforms/micaz/sdr/platform_hardware.h tos/platforms/micaz/sdr/
+mkdir -p $(TOSROOT)/tos/platforms/micaz/sdr/
+cp tos/platforms/micaz/sdr/.platform $(TOSROOT)/tos/platforms/micaz/sdr/
+cp tos/platforms/micaz/sdr/platform_hardware.h $(TOSROOT)/tos/platforms/micaz/sdr/
 
-mkdir -p tos/chips/cc2420/sdr/
-cp $(TOSROOT)/tos/chips/cc2420/sdr/*.nc tos/chips/cc2420/sdr/
+mkdir -p $(TOSROOT)/tos/chips/cc2420/sdr/
+cp tos/chips/cc2420/sdr/*.nc $(TOSROOT)/tos/chips/cc2420/sdr/
 
-mkdir -p tos/chips/cc2420/control/sdr
-cp $(TOSROOT)/tos/chips/cc2420/control/sdr/*.nc tos/chips/cc2420/control/sdr/
+mkdir -p $(TOSROOT)/tos/chips/cc2420/control/sdr
+cp tos/chips/cc2420/control/sdr/*.nc $(TOSROOT)/tos/chips/cc2420/control/sdr/
 
-mkdir -p tos/chips/cc2420/csma/sdr
-cp $(TOSROOT)/tos/chips/cc2420/csma/sdr/*.nc tos/chips/cc2420/csma/sdr/
+mkdir -p $(TOSROOT)/tos/chips/cc2420/csma/sdr
+cp tos/chips/cc2420/csma/sdr/*.nc $(TOSROOT)/tos/chips/cc2420/csma/sdr/
 
-mkdir -p tos/chips/cc2420/packet/sdr
-cp $(TOSROOT)/tos/chips/cc2420/packet/sdr/*.nc tos/chips/cc2420/packet/sdr/
+mkdir -p $(TOSROOT)/tos/chips/cc2420/packet/sdr
+cp tos/chips/cc2420/packet/sdr/*.nc $(TOSROOT)/tos/chips/cc2420/packet/sdr/
 
-mkdir -p tos/chips/cc2420/receive/sdr
-cp $(TOSROOT)/tos/chips/cc2420/receive/sdr/*.nc tos/chips/cc2420/receive/sdr/
+mkdir -p $(TOSROOT)/tos/chips/cc2420/receive/sdr
+cp tos/chips/cc2420/receive/sdr/*.nc $(TOSROOT)/tos/chips/cc2420/receive/sdr/
 
-mkdir -p tos/chips/cc2420/transmit/sdr
-cp $(TOSROOT)/tos/chips/cc2420/transmit/sdr/*.nc tos/chips/cc2420/transmit/sdr/
+mkdir -p $(TOSROOT)/tos/chips/cc2420/transmit/sdr
+cp tos/chips/cc2420/transmit/sdr/*.nc $(TOSROOT)/tos/chips/cc2420/transmit/sdr/
 
-mkdir -p support/make/
-cp $(TOSROOT)/support/make/sdr.extra support/make/
+mkdir -p $(TOSROOT)/support/make/
+cp support/make/sdr.extra $(TOSROOT)/support/make/
 

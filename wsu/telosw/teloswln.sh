a2a30b233da54a29dbc77fdafcdbb2d837408858
wsuvsensorweb <wsuvsensorweb>
2010-03-22 17:24:33 +0000

    no message

--- wsu/telosw/teloswln.sh
+++ wsu/telosw/teloswln.sh
@@ -1,5 +1,5 @@
 sudo ln -s yourlocation/telosw/telosw.target /opt/tinyos-2.x/support/make/telosw.target
 sudo ln -s yourlocation/telosw/teloswplatform /opt/tinyos-2.x/tos/platforms/telosw
-sudo ln -s yourlocationtelosw/ADC /opt/tinyos-2.x/tos/platforms/telosw/chips/ADC
+sudo ln -s yourlocation/telosw/ADC /opt/tinyos-2.x/tos/platforms/telosw/chips/ADC
 sudo ln -s yourlocation/telosw/ccxx00 /opt/tinyos-2.x/tos/chips/ccxx00
 

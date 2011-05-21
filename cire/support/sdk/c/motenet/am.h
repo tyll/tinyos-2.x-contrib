4e8987daab3b30d697451962f794bff4a894cf3b
cire <cire>
2011-05-21 08:11:32 +0000

    Bring in libmotenet from msp430.git/svn trunk motenet_rel branch

--- cire/support/sdk/c/motenet/am.h
+++ cire/support/sdk/c/motenet/am.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011 <your name here>
+ * Copyright (c) 2011 Eric B. Decker.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -14,7 +14,7 @@
  *   documentation and/or other materials provided with the
  *   distribution.
  *
- * - Neither the name of the copyright holders nor the names of
+ * - Neither the name of the copyright holder nor the names of
  *   its contributors may be used to endorse or promote products derived
  *   from this software without specific prior written permission.
  *
@@ -31,3 +31,44 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
  * OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+
+#ifndef __AM_H__
+#define __AM_H__
+
+#include <stdint.h>
+
+#define AM_ENCAP_BASIC	0x00
+#define AM_ENCAP_LEN16	0x80
+
+#define AM_ADDR_BCAST   0xffff
+#define AM_ADDR_ANY     0x0000
+
+#define AM_GRP_ANY      0x00
+
+#define AM_TYPE_ANY	0x00
+
+
+#define AM_HDR_LEN      8
+
+typedef struct __attribute__((__packed__)) {
+  uint8_t  am_encap;
+  uint16_t am_dest;			/* network order */
+  uint16_t am_src;			/* network order */
+  uint8_t  am_len;
+  uint8_t  am_grp;
+  uint8_t  am_type;
+  uint8_t  am_data[0];
+} am_hdr_t;
+
+
+typedef struct __attribute__((__packed__)) {
+  uint8_t  am_encap;
+  uint16_t am_dest;			/* network order */
+  uint16_t am_src;			/* network order */
+  uint16_t am_len;			/* network order */
+  uint8_t  am_type;
+  uint8_t  am_data[0];
+} am_len16_hdr_t;
+
+
+#endif		/* __AM_H__ */

b53d6592c877c7cb3aef2d2b0713e4c40321796e
mthansen <mthansen>
2011-10-25 08:33:43 +0000

    added source script

---
+++ csau/tinyos.sh
@@ -0,0 +1,9 @@
+# TinyOS Environment
+export TOSCSAU=`pwd`
+export PYTHONPATH=$TOSCSAU/misc/support/sdk/python:.
+
+#export TOSDIR=$TOSROOT/tos
+#export CLASSPATH=$TOSROOT/support/sdk/java/tinyos.jar:.
+
+#export MAKERULES=$TOSROOT/support/make/Makerules
+#export PATH=/opt/msp430/bin:$PATH

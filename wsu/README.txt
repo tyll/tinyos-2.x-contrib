5053132d38b027becb5e09aee44125758ee17827
wsuvsensorweb <wsuvsensorweb>
2012-11-29 18:43:36 +0000

    add README to cvs

---
+++ wsu/README.txt
@@ -0,0 +1,13 @@
+
+0. Prerequisite: install tinyos-2.x under /opt/
+
+1. Install TelosW Driver.
+(1) cd telosw
+(2) run teloswln.sh: 
+>source teloswln.sh
+
+2. Verify Telosw Driver.
+(1) cd telosw/apps/RadioCountToLeds
+(2) compile for telosw platform should work: 
+>make telosw
+
--- wsu/telosw/teloswln.sh
+++ wsu/telosw/teloswln.sh
@@ -1,5 +1,5 @@
-sudo ln -s yourlocation/telosw/telosw.target /opt/tinyos-2.x/support/make/telosw.target
-sudo ln -s yourlocation/telosw/teloswplatform /opt/tinyos-2.x/tos/platforms/telosw
-sudo ln -s yourlocation/telosw/ADC /opt/tinyos-2.x/tos/platforms/telosw/chips/ADC
-sudo ln -s yourlocation/telosw/ccxx00 /opt/tinyos-2.x/tos/chips/ccxx00
+sudo ln -s $PWD/telosw.target /opt/tinyos-2.x/support/make/telosw.target
+sudo ln -s $PWD/teloswplatform /opt/tinyos-2.x/tos/platforms/telosw
+sudo ln -s $PWD/ADC /opt/tinyos-2.x/tos/platforms/telosw/chips/ADC
+sudo ln -s $PWD/ccxx00 /opt/tinyos-2.x/tos/chips/ccxx00
 
